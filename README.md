4ad9131c87e70a3a2de6f1b9728ba2796eb56c23
Gregory Wiltshire <mellon.greg@gmail.com>
2020-11-05 00:36:37 -0500

    adds @a

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # airflow-slack-webhook
+- Create a slack app with channel and incoming webhook.
+- Place the slack webhook url in plaintext in your secrets manager secret with the following format:
+```
+https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/abcdefghijklmnopqrstuvwx
+```
+- Populate the environment variable for SLACK_WEBHOOK_SECRET_ID with your secret id.
+- Put slack_webhook.py in your $AIRFLOW_HOME/plugins folder
+- if you wish to @ the owner of the dag populate the owner argument with the slack username(s).
+
+example:
+```python
+from slack_webhook import failure_callback
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'slack.username'
+}
+
+dag = DAG('fail')
+
+fail_task = BashOperator(
+    task_id='always_fails',
+    bash_command='exit 1',
+    on_failure_callback=failure_callback,
+    default_args=default_args,
+    dag=dag,
+)
+```
---
+++ slack_webhook.py
@@ -0,0 +1,29 @@
+import boto3
+from os import environ
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+secret_id = environ['SLACK_WEBHOOK_SECRET_ID']
+client = boto3.client('secretsmanager')
+slack_webhook_url = client.get_secret_value(SecretId=secret_id)['SecretString']
+
+
+def send_message(context, msg):
+    message = SlackWebhookOperator(
+        webhook_token=slack_webhook_url,
+        message=msg,
+        task_id=context['task_instance'].task_id
+    )
+    return message.execute(context=context)
+
+
+def failure_callback(context):
+    owners = str(context['dag'].owner).split(',')
+    ats = ''.join([f'<@{owner}> ' for owner in owners])
+    msg = f"""Task Failed. :yikes:\n
+Dag: {context['task_instance'].dag_id}
+Task: {context['task_instance'].task_id}
+Execution Time: {context['execution_date']}
+<{context['task_instance'].log_url}|View Log>
+{ats}
+"""
+    send_message(context, msg)
